83256e7617dffd869b8d0d22dc78b2bac1e88173
sungmino <no.sung.mi@ncs.com.sg>
2023-05-25 17:23:52 +0700

    feat: deploy success

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-import { User } from './interface/user';
-import { auth, generateJWT } from './util/util';
+import bodyParser from "body-parser";
+import express from "express";
+import { User } from "./interface/user";
+import { auth, deleteLocalFiles, filterImageFromURL, generateJWT } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
@@ -20,20 +19,25 @@ import { auth, generateJWT } from './util/util';
   let users: User[] = [
     {
       id: 1,
-      username: 'test1',
-      password: 123123
-    }
+      username: "test1",
+      password: 123123,
+    },
   ];
 
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
-  app.post('/signup', (req, res) => {
-    const {username, password} = req.body;
+  app.post("/signup", (req, res) => {
+    const { username, password } = req.body;
 
     if (!username || !password) {
-     return res.status(400).send({error: true, message: 'Username and Password must be not empty'});
+      return res
+        .status(400)
+        .send({
+          error: true,
+          message: "Username and Password must be not empty",
+        });
     }
 
     // for (let item of users) {
@@ -43,53 +47,57 @@ import { auth, generateJWT } from './util/util';
     // }
     const checkUser = users.filter((item) => item.username === username);
     if (checkUser.length > 0) {
-      return res
-        .status(409)
-        .send({
-          error: true,
-          message: `User name: ${username} existed. please choice other user name`,
-        });
+      return res.status(409).send({
+        error: true,
+        message: `User name: ${username} existed. please choice other user name`,
+      });
     }
 
     const newUser = {
       username,
       password,
-      id: users.length + 1
-    }
+      id: users.length + 1,
+    };
 
     users.push(newUser);
 
     return res.status(201).send({
       error: false,
-      messgae: 'User register successfully',
-      token: generateJWT(newUser)
+      messgae: "User register successfully",
+      token: generateJWT(newUser),
     });
+  });
 
-  })
-
-  app.post('/login', (req, res) => {
-    const {username, password} = req.body;
+  app.post("/login", (req, res) => {
+    const { username, password } = req.body;
 
     if (!username || !password) {
-     return res.status(400).send({error: true, message: 'Username and Password must be not empty'});
+      return res
+        .status(400)
+        .send({
+          error: true,
+          message: "Username and Password must be not empty",
+        });
     }
 
     const checkUser = users.filter((user) => user.username === username);
 
     if (checkUser.length === 0) {
-      return res.status(404).send({error: true, message: 'User not found'});
+      return res.status(404).send({ error: true, message: "User not found" });
     }
 
     if (checkUser[0].password !== password) {
-      return res.status(400).send({error: true, message: 'Incorrect password'});
+      return res
+        .status(400)
+        .send({ error: true, message: "Incorrect password" });
     }
 
     return res.status(201).send({
       error: false,
-      message: 'Login successfully',
-      token: generateJWT(checkUser[0])
-    })
-  })
+      message: "Login successfully",
+      token: generateJWT(checkUser[0]),
+    });
+  });
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
@@ -105,27 +113,43 @@ import { auth, generateJWT } from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  app.get('/filteredimage', auth(), (req, res) => {
-    const {image_url} = req.query;
-    if (!image_url || image_url.length === 0) {
-      return res.status(400).send({error: true, message: 'You pass an image url to download'});
+  app.get("/filteredimage", auth(), async (req, res) => {
+    try {
+      const image_url = req.query.image_url.toString();
+      if (!image_url || image_url.length === 0) {
+        return res
+          .status(400)
+          .send({ error: true, message: "Image URL is required" });
+      }
+
+      // const response = await axios.head(image_url);
+      // const contentType = response.headers["content-type"];
+      // if (!contentType || !contentType.startsWith("image/")) {
+      //   return res.status(400).send({error: true, message: 'Image url invalid'});
+      // }
+
+      const filterImage = await filterImageFromURL(image_url);
+
+      res.sendFile(filterImage, function () {
+        return deleteLocalFiles([filterImage]);
+      })
+    } catch (error) {
+      console.log('Filtered Image Error: ', error);
+      return res.status(422).send({message: 'Unprocessable Content'})
     }
-  })
+  });
 
   /**************************************************************************** */
 
   //! END @TODO1
 
-
-  app.use('*', (req, res) => {
-    return res.status(404).send({message: 'Endpoint Not Found'})
-  })
-
-
+  app.use("*", (req, res) => {
+    return res.status(404).send({ message: "Endpoint Not Found" });
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
